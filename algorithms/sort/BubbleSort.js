c401f98b527e61e877580b7433f73e5db8af6559
Vaibhav Tripathi <01vatri@gmail.com>
2020-09-30 17:51:20 +0530

    sorting algo added

---
+++ algorithms/sort/BubbleSort.js
@@ -0,0 +1,25 @@
+// @input:      Array and value to search for
+// @output:     Array sorted in ascending / descending order
+// @time-cplx:  O(n2)
+// @spc-cplx:   O(1)
+
+
+// ITERATIVE APPROACH
+
+function bubbleSort(ar){
+    
+    for (var i = ar.length-1; i > 0; i--) {
+        var element = ar[i], pos = i;
+        
+        for(var j = 0; j < i; j++){
+            if(ar[j] > element){
+                element = ar[j];
+                pos = j;
+            }
+        }
+    
+        var tmp = ar[i];
+        ar[i] = ar[pos];
+        ar[pos] = tmp;
+    }
+}
---
+++ algorithms/sort/SelectionSort.js
@@ -0,0 +1,26 @@
+// @input:      Array and value to search for
+// @output:     Array sorted in ascending / descending order
+// @time-cplx:  O(n2)
+// @spc-cplx:   O(1)
+
+
+// ITERATIVE APPROACH
+
+function selectionSort(ar){     //  This function sorts in ascending order
+    
+    for (var i = 0; i < ar.length; i++) {
+        var element = ar[i], pos = i;
+
+        for (let j = i+1; j < ar.length; j++) {
+            if(ar[j] < element){
+                element = ar[j];
+                pos = j;
+            }            
+        }
+
+        var tmp = ar[i];
+        ar[i] = ar[pos];
+        ar[pos] = tmp;
+    }
+}
+
